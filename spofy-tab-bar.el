960c173326aefcde109c25215e2bba02d9cf26e8
Pablo Stafforini <pablo@stafforini.com>
2026-04-01 19:15:27 -0300

    Change default spofy-tab-bar-format to "%p %t %a %s%r"

--- spofy-tab-bar.el
+++ spofy-tab-bar.el
@@ -38,7 +38,7 @@
 
 ;;;; Customization
 
-(defcustom spofy-tab-bar-format "%p %t — %a %s%r"
+(defcustom spofy-tab-bar-format "%p %t %a %s%r"
   "Format string for the Spofy tab-bar segment.
 The following format specifiers are supported:
   %t  track name
