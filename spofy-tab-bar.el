121e48e1c528b501ca542c2c27a576af6de6b10a
Pablo Stafforini <pablo@stafforini.com>
2026-04-02 10:51:02 -0300

    Remove play/pause icon from tab bar default format

--- spofy-tab-bar.el
+++ spofy-tab-bar.el
@@ -38,7 +38,7 @@
 
 ;;;; Customization
 
-(defcustom spofy-tab-bar-format "%p%s%r %t %a"
+(defcustom spofy-tab-bar-format "%s%r %t %a"
   "Format string for the Spofy tab-bar segment.
 The following format specifiers are supported:
   %t  track name
